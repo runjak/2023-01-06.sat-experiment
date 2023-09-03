137284c50e869bcc357cc0512e6b184bb5976206
Jakob Runge <github@runjak.codes>
2023-09-03 20:20:29 +0200

    generate with 5mm tooth size

--- openscad/generate.js
+++ openscad/generate.js
@@ -8,5 +8,5 @@ const generate = (generateTarget, toScale) =>
   $`openscad -D toScale=${toScale} -D generateTarget=${generateTarget} -o ${fileName(generateTarget)} pluscube.scad`
 
 for (let i = 0; i < 27; i++) {
-  generate(i, 3);
+  generate(i, 5);
 }
